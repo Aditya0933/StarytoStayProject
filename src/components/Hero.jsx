ad1ae3e2f5e75b52e7cdc41f5da3345cc9ae2884
Aditya Singh Parihar <201037@kit.ac.in>
2023-08-10 22:03:28 +0530

    Adding Search Bar in Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import beachVid from '../assets/dog.mp4';
-import '../components/Hero.css'
 
 const Hero = () => {
   return (
@@ -15,40 +14,30 @@ const Hero = () => {
       />
       <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'></div>
       <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
-        <h1 className='text-5xl'>Rescue, Rehabilitate, Rehome</h1>
-        <h2 className=' text-xl py-4'> Changing Lives Fur-ever!</h2>
-        {/* <form
+        <h1>Rescue, Rehabilitate, Rehome</h1>
+        <h2 className='py-4'> Changing Lives Fur-ever!</h2>
+        <form
           className='flex justify-between items-center max-w-[700px] mx-auto w-full border p-1
           rounded-md text-black bg-gray-100/90'
         >
-        <div>
-          <input
-            className='bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none
-                '
-            type='text'
-            placeholder='Find Your Paw-some Companion'
-          />
-        </div>
-        <div>
-          <button>
-            <AiOutlineSearch size={20} className='icon' style={{color: '#ffffff'}}  />
-          </button>
-        </div>
-        </form> */}
-
-
-        {/* Adding Button in Home Page */}
-        <div className='flex gap-4 mt-3 justify-center items-center'>
           <div>
-            <button className='btn-1 py-3 px-5 '>Find Out More</button>
+            <input
+              className='bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none
+                  '
+              type='text'
+              placeholder='Find Your Paw-some Companion'
+            />
           </div>
           <div>
-            <button className='btn-2 bg-transparent py-3 px-5 border-yellow-500'> Donate </button>
+            <button>
+              <AiOutlineSearch size={20} className='icon' style={{color: '#ffffff'}}  />
+            </button>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
 };
 
 export default Hero;
+
