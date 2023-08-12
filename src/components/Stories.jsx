7cf070b56bdd0174b65679eb0d2c58d956a70f62
Aditya Singh Parihar <201037@kit.ac.in>
2023-08-13 01:10:58 +0530

    Stories Card Fix

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -11,24 +11,25 @@ import Sto10 from '../assets/10-happy-pet-stories-2017-Two-older-dogs.webp'
 export default function Story(){
     return(
         <div>
-            <div class="flex flex-col py-20 px-10 bg-gray-200">
-                <h1 class="font-bold text-4xl text-gray-800 text-center mb-10">Happy Stories </h1>
+            <div class="flex flex-col py-20 px-1 bg-gray-900 sm:px-10">
+                <h1 class="font-bold text-4xl text-center mb-5 text-white">Happy Stories </h1>
+                <p class="text-xl text-center mb-10 text-white">We are happy that we help families find a new family member for themselves.</p>
                 
-                <div class="flex overflow-x-scroll pb-10 hide-scroll-bar z-50">
-                    <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 z-50 ">
+                <div class="flex overflow-x-scroll pb-10 hide-scroll-bar z-50 mt-5">
+                    <div class="flex flex-nowrap z-50">
                         <div class="inline-block px-3">
-                            <div class="w-64 h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 hover:shadow-xl hover:transition-shadow hover:ease-in-out border-gray-300 border-2 border-solid p-3 hover:border-gray-600 duration-500">
+                            <div class="w-[310px] h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 border-none p-3">
                             <h3 className='text-center mb-2'> A dog named Boy</h3>
-                                <img src={Sto1} alt="" className='p-2'/>
+                                <img src={Sto1} alt="" className='p-2 rounded'/>
                                 <p className='mt-2 p-2'>A Texas family’s tragic year turns hopeful knowing that their dog is safe after Hurricane Harvey.
                                 Boy the dog is more than a pet to his family Boy is more than a pet to his family. When Hurricane Harvey separated the sweet brown-and-white pit bull terrier from the Bonillas, it was devastating on a deeper level than just missing their dog. </p>
                             </div>
                         </div>
                         <div class="inline-block px-3">
-                            <div class="w-64 h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 hover:shadow-xl hover:transition-shadow hover:ease-in-out border-gray-300 border-2 border-solid p-3 hover:border-gray-600 duration-500">
+                            <div class="w-[310px] h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 border-none p-3 ">
                             <h3 className='text-center mb-2'>Priceless Picasso</h3>
-                                <img src={Sto2} alt="" className='p-2'/>
-                                <p className='mt-2 p-2'>Best Friends Network partner spotlight: Why everyone is falling in love with two dogs saved by Luvable Dog Rescue.
+                                <img src={Sto2} alt="" className='p-2 rounded'/>
+                                 <p className='mt-2 p-2'>Best Friends Network partner spotlight: Why everyone is falling in love with two dogs saved by Luvable Dog Rescue.
 
             Picasso and Pablo the dogs are brothers and one has a smile like no other
 
@@ -36,10 +37,10 @@ export default function Story(){
                             </div>
                         </div>
                         <div class="inline-block px-3">
-                            <div class="w-64 h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 hover:shadow-xl hover:transition-shadow hover:ease-in-out border-gray-300 border-2 border-solid p-3 hover:border-gray-600 duration-500">
+                            <div class="w-[310px] h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 border-none p-3 ">
                             <h3 className='text-center mb-2'> Wags to riches</h3>
-                                <img src={Sto3} alt="" className='p-2'/>
-                                <p className='mt-2 p-2'>Patrick Stewart fosters a pit bull for Wags and Walks, and it’s utterly adorable.
+                                <img src={Sto3} alt="" className='p-2 rounded'/>
+                                 <p className='mt-2 p-2'>Patrick Stewart fosters a pit bull for Wags and Walks, and it’s utterly adorable.
 
             Patrick Stewart fosters a pit bull terrier named Ginger for Wags and Walks
 
@@ -47,10 +48,10 @@ export default function Story(){
                             </div>
                         </div>
                         <div class="inline-block px-3">
-                            <div class="w-64 h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 hover:shadow-xl hover:transition-shadow hover:ease-in-out border-gray-300 border-2 border-solid p-3 hover:border-gray-600 duration-500">
+                            <div class="w-[310px] h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 border-none p-3 ">
                             <h3 className='text-center mb-2'>Dog and her puppies safe after Hurricane Harvey</h3>
-                                <img src={Sto4} alt="" className='p-2'/>
-                                <p className='mt-2 p-2'>A mother and her litter of puppies find shelter.
+                                <img src={Sto4} alt="" className='p-2 rounded'/>
+                                 <p className='mt-2 p-2'>A mother and her litter of puppies find shelter.
 
             Wonderful mama dog Sweet Abilene is doing a great job caring for her babies
 
@@ -58,10 +59,10 @@ export default function Story(){
                             </div>
                         </div>
                         <div class="inline-block px-3">
-                            <div class="w-64 h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 hover:shadow-xl hover:transition-shadow hover:ease-in-out border-gray-300 border-2 border-solid p-3 hover:border-gray-600 duration-500">
+                            <div class="w-[310px] h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 border-none p-3 ">
                             <h3 className='text-center mb-2'> Dog reunited with family after hurricane</h3>
-                                <img src={Sto5} alt="" className='p-2'/>
-                                <p className='mt-2 p-2'>Marmaduke got out of his yard just as the flooding hit.
+                                <img src={Sto5} alt="" className='p-2 rounded'/>
+                                 <p className='mt-2 p-2'>Marmaduke got out of his yard just as the flooding hit.
 
             Diana Brown and her family have lost a lot in the past two weeks, but got their dog Marmaduke back
 
@@ -69,10 +70,10 @@ export default function Story(){
                             </div>
                         </div>
                         <div class="inline-block px-3">
-                            <div class="w-64 h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 hover:shadow-xl hover:transition-shadow hover:ease-in-out border-gray-300 border-2 border-solid p-3 hover:border-gray-600 duration-500">
+                            <div class="w-[310px] h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 border-none p-3 ">
                             <h3 className='text-center mb-2'>Hurricane Harvey brings together heroes for pets</h3>
-                                <img src={Sto6} alt="" className='p-2'/>
-                                <p className='mt-2 p-2'>Men and women in uniform volunteer to help pets after disaster.
+                                <img src={Sto6} alt="" className='p-2 rounded'/>
+                                 <p className='mt-2 p-2'>Men and women in uniform volunteer to help pets after disaster.
 
             Men and women in uniform volunteer to help pets after Hurricane Harvey in Houston
 
@@ -80,10 +81,10 @@ export default function Story(){
                             </div>
                         </div>
                         <div class="inline-block px-3">
-                            <div class="w-64 h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 hover:shadow-xl hover:transition-shadow hover:ease-in-out border-gray-300 border-2 border-solid p-3 hover:border-gray-600 duration-500">
-                            <h3 className='text-center mb-2'>Senior German shepherd receives the gift of a new home</h3>
-                                <img src={Sto7} alt="" className='p-2'/>
-                                <p className='mt-2 p-2'>After ending up in a Los Angeles shelter, Sienna faces fears and gets a great new family.
+                            <div class="w-[310px] h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 border-none p-3 ">
+                            <h3 className='text-center mb-2'>German shepherd receives the gift of a new home</h3>
+                                <img src={Sto7} alt="" className='p-2 rounded'/>
+                                 <p className='mt-2 p-2'>After ending up in a Los Angeles shelter, Sienna faces fears and gets a great new family.
 
             Senior German shepherd dog Sienna overcame her fears and landed the perfect family
 
@@ -91,10 +92,10 @@ export default function Story(){
                             </div>
                         </div>
                         <div class="inline-block px-3">
-                            <div class="w-64 h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 hover:shadow-xl hover:transition-shadow hover:ease-in-out border-gray-300 border-2 border-solid p-3 hover:border-gray-600 duration-500">
+                            <div class="w-[310px] h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 border-none p-3 ">
                             <h3 className='text-center mb-2'> Homer’s odyssey</h3>
-                                <img src={Sto8} alt="" className='p-2'/>
-                                <p className='mt-2 p-2'>One of the Sanctuary’s longest residing cats finally gets a home.
+                                <img src={Sto8} alt="" className='p-2 rounded'/>
+                                 <p className='mt-2 p-2'>One of the Sanctuary’s longest residing cats finally gets a home.
 
             After nearly two decades of waiting, Homer the cat has finally been adopted
 
@@ -102,10 +103,10 @@ export default function Story(){
                             </div>
                         </div>
                         <div class="inline-block px-3">
-                            <div class="w-64 h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 hover:shadow-xl hover:transition-shadow hover:ease-in-out border-gray-300 border-2 border-solid p-3 hover:border-gray-600 duration-500">
+                            <div class="w-[310px] h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 border-none p-3 ">
                             <h3 className='text-center mb-2'>  Love letter to a dog</h3>
-                                <img src={Sto9} alt="" className='p-2'/>
-                                <p className='mt-2 p-2'>She was once unwanted and then faced challenges finding the right home. But now the dog named Mrs. Mary Todd Lincoln is loved beyond belief.
+                                <img src={Sto9} alt="" className='p-2 rounded'/>
+                                 <p className='mt-2 p-2'>She was once unwanted and then faced challenges finding the right home. But now the dog named Mrs. Mary Todd Lincoln is loved beyond belief.
 
             Mrs. Mary Todd Lincoln the dog’s adoption story from Best Friends in New York is pure happiness
 
@@ -113,10 +114,10 @@ export default function Story(){
                             </div>
                         </div>
                         <div class="inline-block px-3">
-                            <div class="w-64 h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 hover:shadow-xl hover:transition-shadow hover:ease-in-out border-gray-300 border-2 border-solid p-3 hover:border-gray-600 duration-500">
+                            <div class="w-[310px] h-96 max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-100 border-none p-3 ">
                             <h3 className='text-center mb-2'>Two older dogs, one perfect home</h3>
-                                <img src={Sto10} alt="" className='p-2'/>
-                                <p className='mt-2 p-2'>Adopting two senior shih tzus is the best decision these New Yorkers have ever made.
+                                <img src={Sto10} alt="" className='p-2 rounded'/>
+                                 <p className='mt-2 p-2'>Adopting two senior shih tzus is the best decision these New Yorkers have ever made.
 
             Adopting two senior shih tzus is the best decision these New Yorkers have ever made
 
